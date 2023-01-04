8b44519a4cc9465e642d9e59fbcfe30bb5e39ee5
Aczél Judit <aczel.judit@mav-szk.hu>
2023-01-04 17:13:54 +0100

    Add project files.

---
+++ WiredBrainCoffeeSurveys.Reports/Services/SurveyDataService.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffeeSurveys.Reports.Services
+{
+    class SurveyDataService
+    {
+        public static SurveyResult GetSurveyDataByFileName(string fileName)
+        {
+            return JsonConvert.DeserializeObject<SurveyResult>
+                    (File.ReadAllText($"data/{fileName}.json"));
+        }
+    }
+}
